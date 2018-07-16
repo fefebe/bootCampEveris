169917a5ccec6fa38c983255f32ba6263a8fed37
Unknown <elchico_feli@hotmail.com>
2018-07-16 12:28:32 +0200

    esto no me vale como prueba OJO

---
+++ ProyectoPrueba/src/com/everis/bootcamp/Main.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.everis.bootcamp;
+
+/**
+ * @author operator
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola Paco!");
+		System.err.println("Estas en numeros rojos!!!");
+
+	}
+
+}
